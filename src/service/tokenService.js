a179eb37f63cc840a54dd1ad4da113bfd5c399bc
nguyen-gia-khoi <giakhoi2004@gmail.com>
2024-10-07 19:47:25 +0700

    đăng nhập, xác thực atp, có access token

---
+++ src/service/tokenService.js
@@ -0,0 +1,26 @@
+const redis = require("../config/redis");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (userId) => {
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+  return { accessToken, refreshToken };
+};
+
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refresh_token:${userId}`,
+    refreshToken,
+    "EX",
+    60 * 60 * 24 * 7
+  );
+};
+
+module.exports = {
+  generateToken,
+  storeRefreshToken,
+};
