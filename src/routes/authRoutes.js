4131e39002db413ec2cdbdc5dee94ab1f557041b
nguyen-gia-khoi <giakhoi2004@gmail.com>
2024-10-07 21:24:40 +0700

    feat/signin

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController');
 
 router.post('/signup', authController.signup);
 router.post('/verify-account', authController.verfiaccount)
-router.post("/login", authController.login);
+router.post("/signin", authController.login);
 router.post("/logout", authController.logout);
 router.post("/refresh-token", authController.refreshToken);
 router.post("/forgot-password", authController.forgotPassword)
